c952746eb6997214d4f57f0ebe5d8ce63f53efce
onfranciis <onukwuf@gmail.com>
2024-10-04 08:59:16 +0100

    feat: setup repo and import asset

---
+++ src/utils/fonts.ts
@@ -0,0 +1,66 @@
+import localFont from "next/font/local";
+
+export const harmonyOs = localFont({
+  src: [
+    {
+      path: "../../public/font/HarmonyOS_Sans_Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../../public/font/HarmonyOS_Sans_Black_Italic.ttf",
+      weight: "900",
+      style: "italic",
+    },
+    {
+      path: "../../public/font/HarmonyOS_Sans_Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/font/HarmonyOS_Sans_Bold_Italic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../../public/font/HarmonyOS_Sans_Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/font/HarmonyOS_Sans_Light_Italic.ttf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../../public/font/HarmonyOS_Sans_Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/font/HarmonyOS_Sans_Medium_Italic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../../public/font/HarmonyOS_Sans_Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/font/HarmonyOS_Sans_Regular_Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../public/font/HarmonyOS_Sans_Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/font/HarmonyOS_Sans_Thin_Italic.ttf",
+      weight: "100",
+      style: "italic",
+    },
+  ],
+});
