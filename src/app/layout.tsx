f37639cb2038d4af26b0a546507f283a3bfd7a5e
onfranciis <onukwuf@gmail.com>
2024-10-04 09:17:29 +0100

    feat: create Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { harmonyOs } from "@/utils/fonts";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "Portixel",
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${harmonyOs.className} antialiased`}>{children}</body>
+      <body className={`${harmonyOs.className} antialiased `}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full centerUtil justify-between py-5 px-[82px]">
+      <Link href="">
+        <Image
+          alt="Portixel logo"
+          height={35}
+          width={139}
+          src="/icon/logoFull.svg"
+        />
+      </Link>
+
+      <div className="centerUtil gap-4">
+        {Links.map((link) => (
+          <Link
+            key={link.title}
+            href={link.url}
+            className="px-[22px] py-[10px] rounded-[10px]"
+          >
+            {link.title}
+          </Link>
+        ))}
+
+        <button className="px-[22px] py-[10px] rounded-[10px] bg-white text-black">
+          Try Now
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+const Links = [
+  {
+    title: "Home",
+    url: "",
+  },
+  {
+    title: "Blog",
+    url: "",
+  },
+  {
+    title: "Community",
+    url: "",
+  },
+];
